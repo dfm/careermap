c9516857ba3e0ecc6ed1113ed80cec7d6cd74629
Dan F-M <danfm@nyu.edu>
2013-09-17 15:41:46 -0400

    a little faster

--- careermap/locations.py
+++ careermap/locations.py
@@ -4,13 +4,16 @@
 from __future__ import unicode_literals, division, print_function
 import re
 
+# Country codes.
 codes = dict([line.decode("utf-8").split("\t")[:2]
               for line in open("data/admin1CodesASCII.txt")])
+# zipcodes = dict([(line.split(",")[0].strip(), map(float, line.split(",")[3:4]))
+#                  for line in open("data/zipcode.csv") if "zip" not in line])
 
 
 def load_locations():
-    results = []
-    with open("data/cities1000.txt") as f:
+    locations = []
+    with open("data/cities15000.txt") as f:
         for line in f:
             cols = line.decode("utf-8").split("\t")
             names = [cols[1], cols[2]] + cols[3].split(",")
@@ -21,30 +24,23 @@ def load_locations():
             code = cols[10]
             population = int(cols[14])
             state = codes.get(country_codes[0] + "." + code, None)
-            results.append({
-                "city_names": names,
+            locations.append({
+                "city": names[0],
+                "alternatives": names[1:],
                 "state": state,
                 "country_codes": country_codes,
                 "population": population,
                 "latlng": latlng,
             })
-
-    return results
+    return locations
 
 
 def location_score(affiliation, location):
-    cities = location["city_names"][:50]
-    city_re = re.compile("|".join([r"(\b{0}\b)".format(re.escape(city))
-                                   for city in cities]),
-                         re.I)
-    score = len(city_re.findall(affiliation))
+    srch = location["city"]
+    score = len(re.findall(r"(\b{0}\b)".format(re.escape(srch)), affiliation,
+                           re.I))
     if score == 0:
-        return 0.0
-
-    countries = location["country_codes"][:50]
-    country_re = re.compile("|".join([r"(\b{0}\b)".format(re.escape(c))
-                                      for c in countries]))
-    score += len(country_re.findall(affiliation))
+        return 0
 
     state = location["state"]
     if state is not None:
@@ -57,6 +53,6 @@ def location_score(affiliation, location):
 if __name__ == "__main__":
     locations = load_locations()
     for l in locations:
-        score = location_score("New York", l)
-        if score > 0:
-            print(score, l["city_names"][0] + ", " + l["country_codes"][0])
+        score = location_score("Department of Astronomy and Astrophysics, University of California, Santa Cruz, Santa Cruz, CA 95064", l)
+        if score:
+            print(score, l["city"], l["state"])
