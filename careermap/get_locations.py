f2627a72a0b96b698e76e6b526be9e002067f771
Dan F-M <danfm@nyu.edu>
2013-09-17 18:41:06 -0400

    better location output

--- careermap/get_locations.py
+++ careermap/get_locations.py
@@ -12,12 +12,25 @@ from ads import get_author_locations
 
 def get_locations(name):
     affils = get_author_locations(name)
+    print(affils)
     if not len(affils):
         return []
-    print(affils)
-    locations = map(geocode, affils.values())
-    return locations
+    locations = []
+    for year, affiliation in affils:
+        if year is None:
+            continue
+
+        loc = geocode(affiliation)
+        if loc is None:
+            continue
+
+        locations.append({
+            "year": year,
+            "affiliation": loc["affiliation"],
+            "latlng": loc["latlng"],
+        })
+    return sorted(locations, key=lambda l: int(l["year"]))
 
 
 if __name__ == "__main__":
-    print(get_locations("Foreman-Mackey"))
+    print(get_locations("Angus, R"))
